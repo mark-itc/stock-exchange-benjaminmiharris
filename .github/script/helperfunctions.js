fd5f522f0f6d3da94051145407436555a67dd888
benjaminmiharris <benjmiharris@gmail.com>
2022-10-19 19:36:46 +0300

    Milestone 3 finished and ready to push to main

---
+++ .github/script/helperfunctions.js
@@ -0,0 +1,13 @@
+export async function GETCompanyDetails(url){
+
+    try{
+        const response = await fetch (url); 
+        const results = await response.json();
+        const dataFeed = await results.profile;
+
+        return dataFeed;
+
+    } catch (e) {
+        console.log(e);
+    }
+}
