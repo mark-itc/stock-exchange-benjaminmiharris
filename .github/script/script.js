cf320efdf9e204829e7df01fb40bd2ae397661b2
benjaminmiharris <benjmiharris@gmail.com>
2022-10-21 15:32:03 +0300

    Milestone 6 started

--- .github/script/script.js
+++ .github/script/script.js
@@ -4,21 +4,18 @@ import {GETCompanyDetails} from './helperfunctions.js';
 
 
 
-
-const searchButton = document.getElementById('submit-button'); 
-const searchInput = document.getElementById('search-input');
 const resultsContainer = document.getElementById('results-box'); 
 
 
 
-class StockExchangeCompaniesSearchResult {
+class SearchResults {
     constructor(searchResult){
         this.company = searchResult.name;  
         this.symbol = searchResult.symbol;
         this.exchangeShortName = searchResult.exchangeShortName;
     }
     
-    createResult(companyDetails){
+    renderResults(companyDetails){
 
         
         const resultDiv = document.createElement('li'); 
@@ -57,37 +54,10 @@ class StockExchangeCompaniesSearchResult {
 }
 
 
-searchButton.addEventListener('click', async (e) => {
-    e.preventDefault(); 
-    clearPreviousResults();
-    showLoader();
+// const searchInputField = document.getElementById('search-input');
 
-    const url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${searchInput.value}&amp;limit=10&amp;exchange=NASDAQ`
+// change to 'input' https://dev.to/am20dipi/how-to-build-a-simple-search-bar-in-javascript-4onf
 
-    try {
-
-        const response = await fetch(url); 
-        const results = await response.json(); 
-
-    
-    
-        results.forEach((item) => {
-            const searchResult = new StockExchangeCompaniesSearchResult(item);
-            GETCompanyDetails(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${item.symbol}`).then((response) => {
-            
-                searchResult.createResult(response);    
-            });
-            
-        });
-    
-        hideLoader();
-
-    } catch (e) {
-        console.error("Type1 Error", e)
-    }
-
-
-}); 
 
 
 function clearPreviousResults(){
@@ -104,5 +74,105 @@ function hideLoader() {
     const resultsLoader = document.getElementById('results-loader:active'); 
     resultsLoader.id = 'results-loader'
 }
+
+
+window.onload = () => {
+    const form = new SearchForm (document.getElementById("form"));
+    const results = new SearchResults (document.getElementById('search-results'));
+
+}
+
+
+class SearchForm {
+    constructor(item){
+        this.container = item;
+
+        this.convertToHTML(this.container);
+    }
+
+    convertToHTML(){
+        const container = this.container;
+
+        container.innerHTML = `<form
+        class="container d-flex justify-content-center search-container col-5"
+      >
+        <div class="input-group input-group-lg">
+          <div class="input-group-prepend">
+            <span class="input-group-text nasdaq-logo"
+              ><img
+                src="./images /Event Thumbnails 400x421 @ 1200x1263 (2).png"
+                width="46"
+            /></span>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            id="search-input"
+            placeholder="Search here"
+          />
+    
+          <div class="input-group-append">
+            <button type="submit" class="submit-button" id="submit-button">
+              <span class="input-group-text search"
+                ><i class="fa-solid fa-magnifying-glass search-background"></i
+              ></span>
+            </button>
+          </div>
+        </div>
+      </form>`
+      
+
+
+      this.addSearchEventListener();
+
+    }
+
+
+    addSearchEventListener(){
+
+        const searchButton = document.getElementById('submit-button'); 
+
+        searchButton.addEventListener('click', (e) => {
+            e.preventDefault(); 
+            // showLoader(); 
+            this.onSearch();
+        })
+
+
+    }
+
+
+    async onSearch() {
+
+        const searchInput = document.getElementById('search-input');
+
+        const url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${searchInput.value}&amp;limit=10&amp;exchange=NASDAQ`
+        
+        try {
+    
+            const response = await fetch(url); 
+            const results = await response.json(); 
+
+        
+            results.forEach((item) => {
+                const searchResult = new SearchResults (item);
+                GETCompanyDetails(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${item.symbol}`).then((response) => {
+                
+                    searchResult.renderResults(response);    
+                });
+                
+            });
+        
+            hideLoader();
+    
+        } catch (e) {
+            console.error("Type1 Error", e)
+        }
+    
+    
+    }; 
+
+}
+
 
 
